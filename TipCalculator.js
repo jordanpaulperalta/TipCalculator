252251778543803245b74162f20281c0aad5a44b
jordanpaul <92407629+jordanpaulperalta@users.noreply.github.com>
2023-04-17 16:57:04 -0600

    Add files via upload

--- TipCalculator.js
+++ TipCalculator.js
@@ -1,19 +1,26 @@
 'use strict';
 
-const bill = Number(prompt('Enter a bill payment: '));
-const billValue = new Number(bill);
+const calcBtn = document.getElementById('calcBtn');
+const billInput = document.getElementById('billInput');
 
-if (bill == billValue) {
-  const tipCalc = function (bill) {
-    if (bill <= 50) {
-      console.log(`Your bill is $${bill}. Tip 10% ($${bill * 0.1})`);
-    } else if (() => 51) {
-      console.log(`Your bill is $${bill}. Tip 15% ($${bill * 0.15})`);
-    } else {
-      return;
-    }
-  };
-  tipCalc(bill);
-} else {
-  console.log('You did not enter a valid number. Try again!');
-}
+calcBtn.addEventListener('click', function () {
+  const bill = Number(billInput.value);
+  const billValue = new Number(bill);
+
+  if (bill == billValue) {
+    const tipCalc = function (bill) {
+      if (bill <= 50) {
+        alert(`Your bill is $${bill}. \nRecommended tip: 10% ($${bill * 0.1})`);
+      } else if (() => 51) {
+        alert(
+          `Your bill is $${bill}. \nRecommended tip: 15% ($${bill * 0.15})`
+        );
+      } else {
+        return;
+      }
+    };
+    tipCalc(bill);
+  } else {
+    alert('You did not enter a valid number. Try again!');
+  }
+});
